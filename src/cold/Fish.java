7c29842df93bdf15a7f7868e6ac82a4a172dd2ab
Aleks <feniksizvdv@ukr.net>
2023-06-04 12:30:46 +0300

    describe method fish

--- src/cold/Fish.java
+++ src/cold/Fish.java
@@ -1,21 +1,21 @@
 package cold;
 
 public class Fish extends ColdBlooded {
-
+    // method describes fishing eating
     @Override
     public void Eat() {
         System.out.println("Fish eating");
     }
-
+    // method describes fishing swimming
     public void Swim() {
         System.out.println("Fish swimming");
     }
-
+    // method describes fishing hunting
     @Override
     public void Hunt() {
         System.out.println("Fish hunt");
     }
-
+    // method describes when fish is defensing
     @Override
     public void defenseMechanism() {
         System.out.println("Turn on the defense mechanism");
